dae6485577fc7e593911920e93fa5d9019cf9f85
tyler-k-1 <131286000+tyler-k-1@users.noreply.github.com>
2023-04-20 17:59:08 +0000

    git

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -15,7 +15,7 @@ const Footer = ({}) => {
             </h1>
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
             </h1>
-            <Button type="primary" onClick={() => window.open("https://tylerkraus.myportfolio.com/contact")}>Schedule a call</Button>
+            <Button type="primary" onClick={() => window.open("mailto:tylerkraus1@gmail.com")}>Schedule a call</Button>
             <div className="mt-10">
               <Socials />
             </div>
