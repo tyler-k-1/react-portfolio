bcc755c95071854ea0bd1017bbba964b196c91df
tyler-k-1 <131286000+tyler-k-1@users.noreply.github.com>
2023-04-20 15:46:38 +0000

    git

--- components/Header/index.js
+++ components/Header/index.js
@@ -69,7 +69,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
             >
               {!isBlog ? (
                 <div className="grid grid-cols-1">
-                  <Button onClick={() => window.open("mailto:tylerkraus1@gmail.com")}>Work</Button>
+                  <Button onClick={() => window.open("https://tylerkraus.myportfolio.com/")}>Work</Button>
                   <Button onClick={handleAboutScroll}>About</Button>
                   {showBlog && (
                     <Button onClick={() => router.push("/blog")}>Blog</Button>
@@ -131,7 +131,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
         </h1>
         {!isBlog ? (
           <div className="flex">
-            <Button onClick={handleWorkScroll}>Work</Button>
+            <Button onClick={() => window.open("https://tylerkraus.myportfolio.com/")}>Work</Button>
             <Button onClick={handleAboutScroll}>About</Button>
             {showBlog && (
               <Button onClick={() => router.push("/blog")}>Blog</Button>
