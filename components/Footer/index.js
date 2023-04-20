8d32fe3f57cd178dc4016327dd696f78c39853c0
tyler-k-1 <131286000+tyler-k-1@users.noreply.github.com>
2023-04-20 17:54:25 +0000

    git

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -8,13 +8,12 @@ const Footer = ({}) => {
     <>
       <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
         <div>
-          <h1 className="text-2xl text-bold">Contact.</h1>
+          <h1 className="text-2xl text-bold"></h1>
           <div className="mt-10">
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-              LET&apos;S WORK
+              LET&apos;S CONNECT
             </h1>
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-              TOGETHER
             </h1>
             <Button type="primary" onClick={() => window.open("https://tylerkraus.myportfolio.com/contact")}>Schedule a call</Button>
             <div className="mt-10">
